f3aa10e20aca92504ea8258f42be0242c7958290
prem.rathore <prathore28647@gmail.com>
2023-09-26 10:11:54 +0530

    Mapping features with stepDefinition code

--- cypress/integration/examples/BDD/eCommStepDef.js
+++ cypress/integration/examples/BDD/eCommStepDef.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 import HomePage from "./pageObjects/HomePage"
 import ProductsPage from "./pageObjects/ProductsPage"
 import CartPage from "./pageObjects/CartPage"
@@ -24,4 +24,28 @@ When('I add products to shopping cart', function(){
         });
 
         productsPage.checkoutButton().click()
+})
+
+And('valdiate the total cart amount', function(){
+
+    let sum = 0;
+    cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+
+        const price = $el.text()
+        const numericValue = parseInt(price.split(' ')[1])
+        sum += numericValue;
+
+    })
+
+    // Get the numeric value of the total
+    cy.get('tr td:nth-child(5) strong').then(function (priceEl) {
+
+        const numericValueTotal = Number(priceEl.text().match(/\d+/)[0])
+
+        cy.log(numericValueTotal)
+        cy.log('Sum of numeric values:', sum);
+        // Assert whether the sum and numericValueTotal are equal
+        expect(sum).to.equal(numericValueTotal)
+    })
+
 })
